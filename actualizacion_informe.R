f97887a8b8cfd68ace144a1dcd4326381fda023e
Diego Huaraca <diego.21.phs@hotmail.com>
2015-03-28 15:42:18 -0500

    Update - DB

--- actualizacion_informe.R
+++ actualizacion_informe.R
@@ -12,10 +12,24 @@ library(gdata)
 base <- read.xls("puntajes_ponderados.xlsx", sheet=1, header=TRUE)
 str(base)
 
-head(data_two)
-
 plot(base$Evaluacion.Global.IES)
 
+variables <- c("Codigo", "Evaluacion Global IES", "Academia meta", "Eficiencia Academica meta", "Investigacion meta", "Organizacion meta",
+               "Infraestructura meta", "Posgrado meta", "Carrera docente meta", "Institucionalizacion meta", "Produccion cientifica",	
+               "Remuneraciones meta", "Formacion posgrado", "Dedicacion meta", "Remuneracion TC", "Tiempo completo meta",	
+               "Doctores TC", "Libros revisados por pares", "Gestion interna meta", "Calidad del gasto", "Transparencia meta", 
+               "Biblioteca meta", "Vinculacion colectividad meta", "Reglamentacion meta", "Tasa de retencion inicial pregrado",	
+               "Eficiencia terminal pregrado", "Estabilidad meta", "TIC meta", "Investigacion regional", "Espacios docentes meta",	
+               "Admision a estudios de pregrado", "Porcentaje TC", "Estudiantes por docente TC", "Planificacion de la investigacion",	
+               "Espacios de Bienestar", "Calidad Aulas", "Bienestar meta", "Aulas meta", "Oficinas TC", "Escalafon meta",	
+               "Libros", "Eficiencia terminal posgrado", "Titularidad TC", "Espacio", "Remuneracion MT/TP",	"Posgrado en Formacion", 
+               "Derechos mujeres meta", "Conectividad", "Admision a estudios de posgrado", "Informacion para la evaluacion", 
+               "Horas-clase TC", "Titularidad", "Tiempos parciales y medio tiempo meta", "Transparencia1", "Regimen Academico", 
+               "Accion afirmativa", "Uso del seguimiento a graduados", "Programas de vinculacion", "Rendicion anual de cuentas",
+               "Etica y responsabilidad", "Horas-clase MT/TP", "Salas para MT/TP", "Innovacion Tecnologica", 
+               "Gestion de biblioteca", "Cobertura Estudiantes", "Concurso", "Docencia-Mujeres", "Direccion-Mujeres", 
+               "Consultas por usuario", "Evaluacion", "Escalafon1", "Presupuesto de programas de vinculacion", "oferta", 
+               "mantenimiento", "CA08", "CA05", "CA03", "CA01", "CA02", "CA07", "CA09", "CA10", "region")
 
 ## Reciclamos variables utilizadas vcomo filtros de la base anterior
 #write.table(data_new, file="base_anterior.txt", row.names=FALSE, sep=";", dec=",")
