43f84752b3d62d04879352c77c3f5be5c7a07a1c
Diego Huaraca <diego.21.phs@hotmail.com>
2015-03-30 22:35:41 -0500

    Update

--- actualizacion_informe.R
+++ actualizacion_informe.R
@@ -14,7 +14,7 @@ str(base)
 
 plot(base$Evaluacion.Global.IES)
 
-variables <- c("Codigo", "Evaluacion Global IES", "Academia meta", "Eficiencia Academica meta", "Investigacion meta", "Organizacion meta",
+var <- c("Codigo", "Evaluacion Global IES", "Academia meta", "Eficiencia Academica meta", "Investigacion meta", "Organizacion meta",
                "Infraestructura meta", "Posgrado meta", "Carrera docente meta", "Institucionalizacion meta", "Produccion cientifica",	
                "Remuneraciones meta", "Formacion posgrado", "Dedicacion meta", "Remuneracion TC", "Tiempo completo meta",	
                "Doctores TC", "Libros revisados por pares", "Gestion interna meta", "Calidad del gasto", "Transparencia meta", 
@@ -31,33 +31,30 @@ variables <- c("Codigo", "Evaluacion Global IES", "Academia meta", "Eficiencia A
                "Consultas por usuario", "Evaluacion", "Escalafon1", "Presupuesto de programas de vinculacion", "oferta", 
                "mantenimiento", "CA08", "CA05", "CA03", "CA01", "CA02", "CA07", "CA09", "CA10", "region")
 
-## Reciclamos variables utilizadas vcomo filtros de la base anterior
-#write.table(data_new, file="base_anterior.txt", row.names=FALSE, sep=";", dec=",")
-
-options(stringsAsFactors=FALSE)
-data <- read.csv(file="/Users/Diego/Dropbox/Proyectos/Ceaaces/Datos/macro_criterios.csv", 
-                 header=TRUE, dec=",", sep=";")
-
 library(ggplot2)
-
-var <-c("Institucion", "Codigo", "Academia", "Investigacion", "Infraestructura", "Organizacion",
-        "Eficiencia Academica", "Oferta", "Tipo Mantenimiento", "CA08", "CA05", "CA03", "CA01",
-        "CA02", "CA07", "CA09", "CA10", "Region")
 
 for(i in c(3:7)){
   mypath <- file.path("","Users","Diego","Dropbox","Proyectos","Ceaaces","Actualizacion",
-                      paste(var[i], "_region", ".png", sep = ""))
+                      paste(var[i], "_oferta", ".png", sep = ""))
   png(file=mypath)
-  grafico <- ggplot(data, aes(x=data[,18], y=data[,i], fill=as.factor(data[,18]))) + labs(x=" ") +
+  grafico <- ggplot(base, aes(x=base[,73], y=base[,i], fill=as.factor(base[,73]))) + labs(x=" ") +
     geom_boxplot(outlier.colour="red",outlier.shape=16, outlier.size=4) + labs(fill=" ") + labs(title=var[i]) +
     labs(y="Indicador")+stat_boxplot(geom ='errorbar') + scale_fill_manual(values=colores) +
     theme(legend.position="none") + theme(text = element_text(size=15)) +
-    annotate("text",y=aggregate(data[,i] ~ data[,18], data, function(i) round(median(i),2))[,2],
-             x=c(1,2,3), label=aggregate(data[,i] ~ data[,18], data, function(i) round(median(i),2))[,2], size=6, colour="snow") +
-    annotate("segment",x=0.5, xend=3.5, y=median(data[,i]), yend=median(data[,i]), colour="red2", linetype="dotted", size=1.1)
+    annotate("text",y=aggregate(base[,i] ~ base[,73], base, function(i) round(median(i),2))[,2],
+             x=c(1,2,3), label=str_replace(aggregate(base[,i] ~ base[,73], base, function(i) round(median(i),2))[,2], "0.","0,"), size=6, colour="snow") +
+    annotate("segment",x=0.5, xend=3.5, y=median(base[,i]), yend=median(base[,i]), colour="red2", linetype="dotted", size=1.1)
   print(grafico)
   dev.off()
 }
 
 
+val <- rbind(aggregate(base[,i] ~ base[,73], base, function(i) round(min(i),4))[,2], # min
+aggregate(base[,i] ~ base[,73], base, function(i) round(max(i),4))[,2], # max
+aggregate(base[,i] ~ base[,73], base, function(i) round(sd(i),4))[,2], # sd
+aggregate(base[,i] ~ base[,73], base, function(i) round(IQR(i),4))[,2], # IQR
+aggregate(base[,i] ~ base[,73], base, function(i) round(quantile(i, probs=0.25),4))[,2], # Q1
+aggregate(base[,i] ~ base[,73], base, function(i) round(quantile(i, probs=0.75),4))[,2]) # Q3
+
+result <- cbind(c("min", "max", "sd", "IQR", "Q1", "Q3"), as.data.frame(val))
 
