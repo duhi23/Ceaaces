faa7663b3871e225cb8733ef3875fd6ebd580cad
Diego Huaraca <diego.21.phs@hotmail.com>
2015-04-10 23:50:36 -0500

    Update - Analisis

--- actualizacion_informe.R
+++ actualizacion_informe.R
@@ -81,5 +81,36 @@ source("boxout.R")
 boxplot.with.outlier.label(base[,6]~base[,74], base$Codigo, push_text_right = .6, range = .2,
                            segement_width_as_percent_of_label_dist = 0.35, data=base, spread_text = F)
 
+# Creacion base para analizar los campos CINE
+
+campo1 <- data.frame(subset(base, base$CA01=="SI")[,1:7], campo=rep(1,45))
+campo2 <- data.frame(subset(base, base$CA02=="SI")[,1:7], campo=rep(2,39))
+campo3 <- data.frame(subset(base, base$CA03=="SI")[,1:7], campo=rep(3,53))
+campo5 <- data.frame(subset(base, base$CA05=="SI")[,1:7], campo=rep(5,49))
+campo7 <- data.frame(subset(base, base$CA07=="SI")[,1:7], campo=rep(7,43))
+campo8 <- data.frame(subset(base, base$CA08=="SI")[,1:7], campo=rep(8,35))
+campo9 <- data.frame(subset(base, base$CA09=="SI")[,1:7], campo=rep(9,42))
+campo10 <- data.frame(subset(base, base$CA10=="SI")[,1:7], campo=rep(10,44))
+
+datos <- rbind(campo1, campo2, campo3, campo5, campo7, campo8, campo9, campo10)
+datos$campo <- factor(datos$campo, levels=c(1,2,3,4,5,6,7,8,9,10), 
+                      labels=c("CA01", "CA02", "CA03", "CA04", "CA05", "CA06", "CA07", "CA08", "CA09", "CA10"))
+
+
+for(i in c(2:7)){
+  mypath <- file.path("","Users","Diego","Dropbox","Proyectos","Ceaaces","Actualizacion","Areas",
+                      paste(var[i], "_areas", ".png", sep = ""))
+  png(file=mypath)
+  grafico <- ggplot(datos, aes(x=datos[,8], y=datos[,i], fill=as.factor(datos[,8]))) + labs(x=" ") +
+    geom_boxplot(outlier.colour="red",outlier.shape=16, outlier.size=4) + labs(fill=" ") + labs(title=var[i]) +
+    labs(y="Indicador")+stat_boxplot(geom ='errorbar') + scale_fill_manual(values=colores) +
+    theme(legend.position="none") + theme(text = element_text(size=15)) +
+    #annotate("text",y=aggregate(datos[,i] ~ datos[,8], datos, function(i) round(median(i),2))[,2],
+    #         x=c(1,2,3), label=str_replace(aggregate(datos[,i] ~ datos[,8], datos, function(i) round(median(i),2))[,2], "0.","0,"), size=6, colour="snow") +
+    annotate("segment",x=0.5, xend=4.5, y=median(datos[,i]), yend=median(datos[,i]), colour="red2", linetype="dotted", size=1.1)
+  print(grafico)
+  dev.off()
+}
+
 
 
