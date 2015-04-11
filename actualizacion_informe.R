88bd5e1aa0cf568fe1397c4fb217054cce939591
Diego Huaraca <diego.21.phs@hotmail.com>
2015-04-11 00:26:35 -0500

    Update

--- actualizacion_informe.R
+++ actualizacion_informe.R
@@ -42,7 +42,7 @@ base$intervalos <- cut(base$nestudiantes, breaks=c(0,2000,6062,13320,73032), dig
 library(ggplot2)
 library(stringr)
 
-colores <- c("green4", "deepskyblue3", "goldenrod3", "gold", "yellow", "greenyellow", "orange", "orangered", "red3" )
+colores <- c("green4", "deepskyblue3", "goldenrod3", "gold", "darkorange", "deepskyblue1", "orange", "orangered", "hotpink" )
 
 for(i in c(2:72)){
   mypath <- file.path("","Users","Diego","Dropbox","Proyectos","Ceaaces","Actualizacion","Oferta",
@@ -93,24 +93,41 @@ campo9 <- data.frame(subset(base, base$CA09=="SI")[,1:7], campo=rep(9,42))
 campo10 <- data.frame(subset(base, base$CA10=="SI")[,1:7], campo=rep(10,44))
 
 datos <- rbind(campo1, campo2, campo3, campo5, campo7, campo8, campo9, campo10)
-datos$campo <- factor(datos$campo, levels=c(1,2,3,4,5,6,7,8,9,10), 
+datos$ncampo <- factor(datos$campo, levels=c(1,2,3,4,5,6,7,8,9,10), 
                       labels=c("CA01", "CA02", "CA03", "CA04", "CA05", "CA06", "CA07", "CA08", "CA09", "CA10"))
+
 
 
 for(i in c(2:7)){
   mypath <- file.path("","Users","Diego","Dropbox","Proyectos","Ceaaces","Actualizacion","Areas",
                       paste(var[i], "_areas", ".png", sep = ""))
   png(file=mypath)
-  grafico <- ggplot(datos, aes(x=datos[,8], y=datos[,i], fill=as.factor(datos[,8]))) + labs(x=" ") +
+  grafico <- ggplot(datos, aes(x=datos[,9], y=datos[,i], fill=as.factor(datos[,9]))) + labs(x=" ") +
     geom_boxplot(outlier.colour="red",outlier.shape=16, outlier.size=4) + labs(fill=" ") + labs(title=var[i]) +
     labs(y="Indicador")+stat_boxplot(geom ='errorbar') + scale_fill_manual(values=colores) +
     theme(legend.position="none") + theme(text = element_text(size=15)) +
-    #annotate("text",y=aggregate(datos[,i] ~ datos[,8], datos, function(i) round(median(i),2))[,2],
-    #         x=c(1,2,3), label=str_replace(aggregate(datos[,i] ~ datos[,8], datos, function(i) round(median(i),2))[,2], "0.","0,"), size=6, colour="snow") +
-    annotate("segment",x=0.5, xend=4.5, y=median(datos[,i]), yend=median(datos[,i]), colour="red2", linetype="dotted", size=1.1)
+    annotate("segment",x=0.5, xend=8.5, y=median(datos[,i]), yend=median(datos[,i]), colour="red2", linetype="dotted", size=1.1)
   print(grafico)
   dev.off()
 }
 
 
+esta <- function(i){
+  val <- rbind(aggregate(datos[,i] ~ datos[,8], datos, function(i) round(min(i),4))[,2], # min
+               aggregate(datos[,i] ~ datos[,8], datos, function(i) round(max(i),4))[,2], # max
+               aggregate(datos[,i] ~ datos[,8], datos, function(i) round(median(i),4))[,2], # median
+               aggregate(datos[,i] ~ datos[,8], datos, function(i) round(sd(i),4))[,2], # sd
+               aggregate(datos[,i] ~ datos[,8], datos, function(i) round(IQR(i),4))[,2], # IQR
+               aggregate(datos[,i] ~ datos[,8], datos, function(i) round(quantile(i, probs=0.25),4))[,2], # Q1
+               aggregate(datos[,i] ~ datos[,8], datos, function(i) round(quantile(i, probs=0.75),4))[,2]) # Q3
+  result <- cbind(c("min", "max", "median", "sd", "IQR", "Q1", "Q3"), as.data.frame(val))
+  colnames(result) <- c("nom", unlist(dimnames(table(datos[,8]))))
+  return(result)
+}
+
+esta(3)
+
+# Valores atipicos
+boxplot.with.outlier.label(datos[,3] ~ datos[,8], datos$Codigo, push_text_right = .6, range = .2,
+                           segement_width_as_percent_of_label_dist = 0.35, data=datos, spread_text = F)
 
