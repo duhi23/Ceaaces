4248dcb39c387e2c28000054b1d0ace18988449c
Diego Huaraca <diego.21.phs@hotmail.com>
2015-08-17 23:35:04 -0500

    Update

--- modelo_econometrico.R
+++ modelo_econometrico.R
@@ -39,25 +39,26 @@ modelo <- lm(VALORACION ~ FORMACIONPOSGRADO + HORASCLASESMTTP + CONCURSO + log(R
                log(CONECTIVIDAD+0.1)  + sqrt(COBERTURAAESTUDIANTES) + OFICINASMTTP + sqrt(PRODUCCIONCIENTIFICA), data)
 summary(modelo)
 
-
 data <- data %>% mutate(EST= -0.5961489 + 0.0024294*FORMACIONPOSGRADO -0.0048324*HORASCLASESMTTP + 0.0006680*CONCURSO +0.1101033*log(REMUNERACIONTC)+
                           0.1372203*sqrt(LIBROSREVISADOSPORPARES) + 0.0315445*log(CONECTIVIDAD + 0.1) + 0.0073168*sqrt(COBERTURAAESTUDIANTES) -0.0019802*OFICINASMTTP +
                           0.0458477*sqrt(PRODUCCIONCIENTIFICA), dif= abs(EST-VALORACION))
 
-data <- data %>% mutate(NEW_CAL=ifelse(round(EST,2) >= 0.6, "A", ifelse(round(EST,2) >= 0.4, "B", ifelse(round(EST,2) >= 0.35, "C", "D"))))
+data <- data %>% mutate(NEW_CAL=ifelse(round(EST,2) >= 0.6, "A", ifelse(round(EST,2) >= 0.45, "B", ifelse(round(EST,2) >= 0.35, "C", "D"))))
 
 table(data$CATEGORIA, data$NEW_CAL)
 
 dife <- data %>% mutate(dife = EST-VALORACION) %>% select(dife)
 plot(cbind(seq(1,54), dife), ylim=c(-0.1, 0.1), xlim=c(0,60), xlab="IES", ylab="Diferencia")
 
+# Resumen diferencia
+dife %>% summary()
+
+# Estadisticos por Categoria
+by(data$VALORACION, data$CATEGORIA, summary)
+
+data %>% select(VALORACION, CATEGORIA) %>% filter(CATEGORIA=="B") %>% head(20)
 
 data %>% select(VALORACION, EST, CATEGORIA, NEW_CAL) %>% tail(30)
 
 ## Error en la estimacion
-1-(5+21+5+5)/54
-
-
-
-
-
+1-(5+23+10)/54
